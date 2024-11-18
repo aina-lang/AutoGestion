1feebd402d736b2d95adec892c7b2ae4a4861328
aina-lang <merciaaina@gmail.com>
2024-11-18 10:26:05 -0500

    gg

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
-
-echo "Running composer install..."
+echo "Running composer"
+composer global require hirak/prestissimo
+composer require tightenco/ziggy
 composer install --no-dev --working-dir=/var/www/html
+composer require 
+echo "generating application key..."
+php artisan key:generate --show
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
-php artisan config:cache
-php artisan route:cache
