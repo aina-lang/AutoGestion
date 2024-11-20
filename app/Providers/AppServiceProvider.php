40faf0c9456d23c3f81427a6be64111bc3f2af65
aina-lang <merciaaina@gmail.com>
2024-11-20 05:40:02 -0500

    ee

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,10 +27,22 @@ class AppServiceProvider extends ServiceProvider
     public function boot(UrlGenerator $url): void
     {
 
-       
+
         Schema::defaultStringLength(191);
-    
-        
+
+
+        $destinationPath = '/var/task/database/database.sqlite';  // Destination dans Vercel
+
+        // Vérifier si le fichier source existe et le fichier de destination n'existe pas
+        if (!File::exists($destinationPath)) {
+
+            // Si le fichier source n'existe pas, créer un fichier SQLite vide à la destination
+            File::put($destinationPath, '');
+            File::put("database/database.sqlite", '');
+            File::put("/tmp/database/database.sqlite", ''); // Créer un fichier vide
+            echo "Le fichier SQLite a été créé avec succès!";
+        }
+
         if (env('APP_ENV') == 'production') {
             $url->forceScheme('https');
         }
